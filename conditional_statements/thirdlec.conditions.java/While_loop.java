6fd52a07a5be8c2c2c8d94f1d9b4c2bae40f4006
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-06-29 22:13:37 +0530

    Add files via upload

---
+++ conditional_statements/thirdlec.conditions.java/While_loop.java
@@ -0,0 +1,10 @@
+
+public class While_loop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 11) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
---
+++ conditional_statements/thirdlec.conditions.java/dowhile_loop.java
@@ -0,0 +1,10 @@
+public class dowhile_loop {
+    public static void main(String[] args) {
+        int i = 11;
+        do {
+            System.out.println(i);
+            i = i + 1;
+        } while (i < 21);
+    }
+
+}
