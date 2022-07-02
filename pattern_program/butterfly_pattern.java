5933fe9a7078aad3d5f3e318ab6af8400b4054d4
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-02 19:49:19 +0530

    Add files via upload

---
+++ pattern_program/butterfly_pattern.java
@@ -0,0 +1,39 @@
+//Butterfly Pattern
+
+import java.util.*;
+
+public class butterfly_pattern {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the no. of rows::");
+        int r = scan.nextInt();
+        // Upper half
+        for (int i = 1; i <= r; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (r - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // Lower half
+        for (int i = r; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (r - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+
+}
