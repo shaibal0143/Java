3d1fbf43bd695e3de1c66974f48c4a0b319650b5
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-14 10:08:49 +0530

    Add files via upload

---
+++ Arrays_in_java/jagged_array.java
@@ -0,0 +1,22 @@
+public class jagged_array {
+    public static void main(String[] args) {
+        int r = 5;
+        int count = 1;
+        int arr[][] = new int[r][];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = new int[i + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = count++;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+
+    }
+}
