d8a07c59a39c1ef7d861b48adfdb74958f53f652
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-24 00:33:57 +0530

    Add files via upload

--- Strings_in_java/substring.java
+++ Strings_in_java/substring.java
@@ -1,16 +1,32 @@
 
-//Substring in java
+//String Builder in java
 import java.util.*;
 
 public class substring {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the Sentence::");
-        String sen = scan.nextLine();
-        System.out.println("The sentence is::" + sen);
-        System.out.println(sen.substring(1, 5));
-        System.out.println(sen.substring(10, sen.length()));
+        System.out.println("Enter your first name:::");
+        StringBuilder str = new StringBuilder(scan.nextLine());
+        System.out.println(str);
+
+        // finding character at an index
+        System.out.println(str.charAt(0));
+
+        // replacing a character at any index using(set charat)
+        str.setCharAt(0, 'p');
+        System.out.println(str);
+
+        // inserting an character at any index using(insert)
+        str.insert(2, 'p');
+        System.out.println(str);
+
+        // Delete a character using(delete)
+        str.delete(2, 3);
+        System.out.println(str);
+
+        str.setCharAt(0, 's');
+        System.out.println(str);
         scan.close();
     }
+
 }
