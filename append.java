46943b4920fd2ca3f8447e3d7feb8b47a31d07f3
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-07-24 00:34:18 +0530

    Add files via upload

---
+++ append.java
@@ -0,0 +1,14 @@
+
+//use of append function in java
+public class append {
+    public static void main(String[] args) {
+
+        StringBuilder str = new StringBuilder("h");
+        str.append("e");
+        str.append("l");
+        str.append("l");
+        str.append("o");
+        System.out.println(str + " Shaibal");
+
+    }
+}
