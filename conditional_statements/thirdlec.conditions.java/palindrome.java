1aa1635ac93b01b31989f3432a1f17ae33215716
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-02 20:21:24 +0530

    Add files via upload

---
+++ conditional_statements/thirdlec.conditions.java/palindrome.java
@@ -0,0 +1,17 @@
+class palindrome{
+	public static void main(String[] args){
+	int n=Integer.parseInt(args[0]);
+	int rem,sum=0,temp;
+	temp=n;
+	while(n>0){
+	rem=n%10;
+	sum=(sum*10)+rem;
+	n=n/10;
+}
+if(temp==sum){
+System.out.println("Palindrome Number...");
+}else{
+	System.out.println("Not a Palindrome Number...");
+	}
+	}
+}
