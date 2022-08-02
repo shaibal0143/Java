83759a3b87a6ec63eb4b850de1d6135859783650
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-02 20:17:38 +0530

    Add files via upload

---
+++ conditional_statements/thirdlec.conditions.java/sorting.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class sorting {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the numbers::");
+
+        int arr[] = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
