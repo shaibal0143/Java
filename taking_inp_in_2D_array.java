aaa6ca641d5ceaf9f0f84e60240e97758eb8cf54
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-10 08:14:28 +0530

    Update taking_inp_in_2D_array.java

--- taking_inp_in_2D_array.java
+++ taking_inp_in_2D_array.java
@@ -20,7 +20,7 @@ public class taking_inp_in_2D_array {
             for (int j = 0; j < col; j++) {
                 System.out.print(num[i][j] + " ");
 
-            }
+             }
             System.out.println();
         }
         scan.close();
