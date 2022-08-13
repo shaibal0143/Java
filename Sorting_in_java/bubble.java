a98f9b5811f41787a8e1bf42bf1a818ee0f0289f
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-13 07:53:20 +0530

    Add files via upload

--- Sorting_in_java/bubble.java
+++ Sorting_in_java/bubble.java
@@ -1,29 +1,20 @@
-import java.util.Scanner;
-
-public class bubble {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the no of Elements you wnat to enter::");
-        int n = scan.nextInt();
-        System.out.println("Enter the Elements::");
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scan.nextInt();
-        }
-        // Bubble sort
-        // Time complexity=O(n^2)
+//sorting using bubble sort
+ class bubble {
+     public static void main(String[] args) {
+        int arr[] = { 44, 22, 33, 99, 66 };
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[i] > arr[i + 1]) {
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
+
                 }
+
             }
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+            System.out.print(arr[i] + " ");
         }
-        scan.close();
     }
 }
