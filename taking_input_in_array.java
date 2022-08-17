91529a8ab83a4ace0880872b30ac3027b9d2f82b
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-08-17 20:01:34 +0530

    Update taking_input_in_array.java

--- taking_input_in_array.java
+++ taking_input_in_array.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class taking_input_in_array {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner  scan = new Scanner(System.in);
         System.out.println("Enter the size of array::");
         int size = scan.nextInt();
         String[] names = new String[size];
