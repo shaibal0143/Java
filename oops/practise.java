c2d8ab3c94ecbe8d69809f5f318d80a99c858fb6
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-10-11 21:50:45 +0530

    Add files via upload

---
+++ oops/practise.java
@@ -0,0 +1,29 @@
+package javaProgram.oops;
+
+class Employee1 {
+    int salary;
+    String name;
+
+    public int getsalary() {
+        return salary;
+    }
+
+    public String getname() {
+        return name;
+
+    }
+
+}
+
+public class practise {
+    public static void main(String[] args) {
+        // problem 1(lecture no. 39 -by Code with harry)
+        Employee1 shaibal = new Employee1();
+        shaibal.salary = 12000;
+        shaibal.name = "Shaibal Kumar Pathak";
+        System.out.println(shaibal.getsalary());
+        System.out.println(shaibal.getname());
+
+    }
+
+}
