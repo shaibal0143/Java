41b8aa6b120d0249957b5d121eadf5cafe56ad2b
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-12-02 11:16:50 +0530

    Update floyds_triangle.java

--- pattern_program/floyds_triangle.java
+++ pattern_program/floyds_triangle.java
@@ -10,7 +10,7 @@ public class floyds_triangle {
         int r = scan.nextInt();
         for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(num + " ");
+                System.out.print(num + " "); 
                 num++;
 
             }
