303f8ce7907ca22af0c2ea5d57db32b140276ac2
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-12-02 11:16:56 +0530

    Update floyds_triangle.java

--- pattern_program/floyds_triangle.java
+++ pattern_program/floyds_triangle.java
@@ -7,7 +7,7 @@ public class floyds_triangle {
         Scanner scan = new Scanner(System.in);
         int num = 1;
         System.out.print("Enter the no.of rows:");
-        int r = scan.nextInt();
+        int r = scan.nextInt(); 
         for (int i = 1; i <= r; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(num + " "); 
