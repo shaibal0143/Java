99db52983293e726077fc88f942d33748d535fb6
Shaibal Kumar Pathak <104377565+shaibal0143@users.noreply.github.com>
2022-12-04 22:19:31 +0530

    Update bubble.java

--- Sorting_in_java/bubble.java
+++ Sorting_in_java/bubble.java
@@ -4,7 +4,7 @@
         int arr[] = { 44, 22, 33, 99, 66 };
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[i] > arr[i + 1]) {
+                if (arr[i] > arr[i + 1]) { 
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
