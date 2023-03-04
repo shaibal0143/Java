6f647ef85b66d6cda85b7be5c77e160aad82f1cb
skp <104377565+shaibal0143@users.noreply.github.com>
2023-03-04 16:40:07 +0530

    Update helloworld.java

--- Basics/first_lecture/helloworld.java
+++ Basics/first_lecture/helloworld.java
@@ -2,7 +2,7 @@ package first_lecture;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("helllo world");
     }
 
 }
