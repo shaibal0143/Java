03db05bcee5b9037e67214f1852401ab44e24f0b
skp <104377565+shaibal0143@users.noreply.github.com>
2023-04-28 09:03:43 +0530

    Update Taking_input.java

--- Strings_in_java/Taking_input.java
+++ Strings_in_java/Taking_input.java
@@ -6,7 +6,7 @@ public class Taking_input {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name::");
-        String name = sc.nextLine();
+        String name = sc.nextLine(); 
         System.out.println("Your name is ::" + name);
         sc.close();
     }
