f641a34f578ab3be5591037ef0d1955572b75722
skp <104377565+shaibal0143@users.noreply.github.com>
2023-05-15 09:36:14 +0530

    Update taking_input_in_array.java

--- taking_input_in_array.java
+++ taking_input_in_array.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class taking_input_in_array {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the size of array::");
+        System.out.println("Enter the size of array::"); 
         int size = scan.nextInt();
         String[] names = new String[size];
         for (int i = 0; i < size; i++) {
