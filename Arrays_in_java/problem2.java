d365ad8088c446228a9128b9615ef8ca44e9293c
skp <104377565+shaibal0143@users.noreply.github.com>
2023-07-16 08:36:35 +0530

    Add files via upload

--- Arrays_in_java/problem2.java
+++ Arrays_in_java/problem2.java
@@ -1,10 +1,25 @@
 package javaProgram.array_revise;
+
 import java.util.*;
 
 public class problem2 {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Size of the Array::");
+        int n = scan.nextInt();
+        String name[] = new String[n];
+        // System.out.print( "-Enter your name::");
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + 1 + "-Enter your name::");
+            name[i] = scan.next();
+
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + 1 + "-The name is::" + name[i]);
+
+        }
+        scan.close();
+
     }
-    
+
 }
